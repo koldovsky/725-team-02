e1422be40ddb11bf07b3293917da8f95bf7d6904
VasylSurmenko <vasylsurmenko@gmail.com>
2022-12-04 16:10:32 +0200

    changed about-us.js file

---
+++ js/about-us.js
@@ -0,0 +1,44 @@
+Deleted part js
+
+(function () {
+ const bookingSteps = [
+    {
+        id: "1",
+        image: "img/first_step_img.png",
+        title: "Contact us",
+        chapter: "Some tour conditions may be changed according to the season. To learn more about the program, please contact us."
+    }
+    {
+        id: "2",
+        image: "img/second_step_img.png",
+        title: "Pick a date",
+        chapter: "We conduct tours all year round, almost every week, but the most comfortable weather for sightseeing is in summer."
+    }
+    {
+        id: "3",
+        image: "img/third_step_img.png",
+        title: "Start your journey",
+        chapter: "If necessary, we help you apply for a visa to Norway, as well as advise what things you should take with you."
+    }
+ ];
+ function renderSteps (steps) {
+    const stepsContainer = document.querySelector('.booking-steps');
+    for (const step of steps) {
+        stepsContainer.innerHTML += `
+        <div class="booking-steps-card">
+              <img
+                class="step-number__image"
+                src="${step.image}"
+                alt="step number"
+              />
+              <h3 class="booking-steps-title">${step.title}</h3>
+              <p class="booking-steps-chapter">${step.chapter}
+              </p>
+            </div>
+        
+        
+        `
+    }
+ }
+ renderSteps()
+})();
