39c965088e15912490b46718752f212453ac8afb
MELL85 <116827471+MELL85@users.noreply.github.com>
2022-12-08 11:34:34 +0000

    change class faq accordion

--- js/faq-accordion.js
+++ js/faq-accordion.js
@@ -8,21 +8,21 @@
             console.log(panel);
             let coursePanel = document.getElementsByClassName('faq__panel');
             console.log(coursePanel);
-            let courseAccordionActive = document.getElementsByClassName('faq__accordion-button active');
+            let courseAccordionActive = document.getElementsByClassName('faq__accordion-button faq__active');
 
             if (panel.style.maxHeight) {
                 panel.style.maxHeight = null;
-                this.classList.remove('active');
+                this.classList.remove('faq__active');
             } else {
                 for (let j = 0; j < courseAccordionActive.length; j++) {
-                    courseAccordionActive[j].classList.remove('active');
+                    courseAccordionActive[j].classList.remove('faq__active');
                 }
                 for (let t = 0; t < coursePanel.length; t++) {
-                    this.classList.remove('active');
+                    this.classList.remove('faq__active');
                     coursePanel[t].style.maxHeight = null;
                 }
                 panel.style.maxHeight = panel.scrollHeight + 'px';
-                this.classList.add('active');
+                this.classList.add('faq__active');
             }
         }
     }
